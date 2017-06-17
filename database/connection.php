8575f779d5d365d95f4d32d434fe8ada2de791b8
cristianRiveragomez <cristianriveragomez95@gmail.com>
2017-06-17 17:16:30 -0300

    se modifico html y se empieza proyecto

---
+++ database/connection.php
@@ -0,0 +1,9 @@
+<?
+
+	//  ------------------------------ Connection Data
+	$hostname = 'localhost';
+	$database = 'ecommerce';
+	$username = 'davinci';
+	$password = 'd4v1nc1';
+
+?>
--- header.php
+++ header.php
@@ -9,19 +9,20 @@
 <!-- Head -->
 <head>
 
-<title>Ecommerce - Da Vinci </title>
+<title>Davinci Ecommerce | Home :: W3layouts</title>
 
 <!-- Meta-Tags -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="keywords" content="Ecommerce - Da Vinci">
+<meta name="keywords" content="Groovy Apparel a Responsive Web Template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web Template, Smartphone Compatible Web Template, Free Webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- //Meta-Tags -->
 
-<!-- Custom-Stylesheet-Links -->
-<!-- Bootstrap-CSS -->	<link rel="stylesheet" href="css/bootstrap.css"	type="text/css" media="all">
-<!-- Index-Page-CSS -->	<link rel="stylesheet" href="css/style.css"		type="text/css" media="all">
-<!-- //Custom-Stylesheet-Links -->
+<!-- Custom-StyleSheet-Links -->
+<!-- Bootstrap-CSS -->	   <link rel="stylesheet" href="css/bootstrap.css"					type="text/css" media="all">
+<!-- Index-Page-CSS -->	   <link rel="stylesheet" href="css/style.css"						type="text/css" media="all">
+<!-- Header-Slider-CSS --> <link rel="stylesheet" href="css/fluid_dg.css" id="fluid_dg-css" type="text/css" media="all">
+<!-- //Custom-StyleSheet-Links -->
 
 <!-- Fonts -->
 <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Serif:400,700"	   type="text/css" media="all">
@@ -33,6 +34,11 @@
 <!-- CSS --> <link rel="stylesheet" href="css/font-awesome.css" 		 type="text/css" media="all">
 <!-- TTF --> <link rel="stylesheet" href="fonts/fontawesome-webfont.ttf" type="text/css" media="all">
 <!-- //Font-Awesome-File-Links -->
+
+<!-- Supportive-Modernizr-JavaScript --><script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+<!-- Default-JavaScript --><script src="js/jquery-2.2.3.js"></script>
+
 </head>
 <!-- //Head -->
 
@@ -44,7 +50,7 @@
 
 
 	<!-- Header -->
-	<div class="agileheader">
+	<div class="agileheader" id="agileitshome">
 
 		<!-- Navigation -->
 		<nav class="navbar navbar-default w3ls navbar-fixed-top">
@@ -56,7 +62,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand agileinfo" href="index.html"><span>Ecommerce</span> DAVINCI</a>
+					<a class="navbar-brand agileinfo" href="index.html"><span>Ecommerce</span> DAVINCI</a> 
 					<ul class="w3header-cart">
 						<li class="wthreecartaits"><span class="my-cart-icon"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i><span class="badge badge-notify my-cart-badge"></span></span></li>
 					</ul>
@@ -264,7 +270,7 @@
 			</div>
 
 				<!-- Popup-Box -->
-				<div id="popup">
+				<div id="popup1">
 					<div id="small-dialog1" class="mfp-hide agileinfo">
 						<div class="pop_up">
 							<form action="#" method="post">
@@ -307,342 +313,3 @@
 		<!-- //Header-Top-Bar-(Hidden) -->
 
 
-
-		<!-- Banner -->
-		<div class="agileheader-banner">
-			<img src="images/banner.jpg" alt="Groovy Apparel">
-		</div>
-		<!-- //Banner -->
-
-	</div>
-	<!-- //Header -->
-
-
-
-	<!-- About-Us -->
-	<div class="wthreeabout">
-		<div class="about-us-grids">
-			<div class="col-md-6 about-us-grid about-us-images">
-				<div class="col-md-6 about-us-image about-us-image1">
-					<img src="images/about-image-1.jpg" alt="Groovy Apparel">
-				</div>
-				<div class="col-md-6 about-us-image about-us-image2">
-					<img src="images/about-image-2.jpg" alt="Groovy Apparel">
-				</div>
-				<div class="col-md-6 about-us-image about-us-image3">
-					<img src="images/about-image-3.jpg" alt="Groovy Apparel">
-				</div>
-				<div class="col-md-6 about-us-image about-us-image4">
-					<img src="images/about-image-4.jpg" alt="Groovy Apparel">
-				</div>
-				<div class="clearfix"></div>
-			</div>
-
-			<div class="col-md-6 about-us-grid about-us-text">
-				<h1>ABOUT US</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo varius vehicula. Mauris porta metus vitae nulla dignissim egestas. Aliquam sed molestie eros, in pharetra purus. Praesent consequat porta viverra. Praesent gravida dapibus condimentum. Vestibulum vel lacus aliquet, laoreet mi eu, tempor est. Quisque volutpat finibus tristique. Duis at nibh eget nulla pretium pretium. Quisque arcu sem, dignissim eu egestas quis, vulputate non mauris. Nam varius, odio vel interdum interdum, lectus ante imperdiet odio, id consectetur mauris quam et lectus. Ut auctor nec metus egestas auctor. Nullam congue ornare dictum. Sed et volutpat lectus. Integer vehicula pharetra ligula, in vehicula magna vehicula eu. Mauris at tortor vel eros faucibus feugiat. In hac habitasse platea dictumst. Phasellus molestie pretium mi id finibus. Nam et purus enim. Morbi nec fermentum enim. Suspendisse potenti. Sed tincidunt augue in pretium lobortis. Donec laoreet nunc a nisi vulputate pulvinar. Morbi aliquam, mi ultricies efficitur feugiat, risus magna id consectetur mauris quam et lectus.</p>
-			</div>
-			<div class="clearfix"></div>
-		</div>
-	</div>
-	<!-- //About-Us -->
-
-
-
-	<!-- Be-Yourself -->
-	<div class="aitsyourself">
-		<div class="aitsyourself-grids">
-
-			<div class="col-md-5 aitsyourself-grid aitsyourself-grid1">
-				
-			</div>
-
-			<div class="col-md-3 aitsyourself-grid aitsyourself-grid2">
-				<h3>“The appearance of things changes according to the emotions; and thus we see magic and beauty in them, while the magic and beauty are really in ourselves.”</h3>
-				<h4>― Kahlil Gibran</h4>
-			</div>
-
-			<div class="col-md-4 aitsyourself-grid aitsyourself-grid3">
-				
-			</div>
-			<div class="clearfix"></div>
-
-		</div>
-	</div>
-	<!-- //Be-Yourself -->
-
-
-
-	<!-- Commercial -->
-	<div class="aitscommercial">
-			
-		<div class="col-md-6 aitscommercial-grid aitscommercial-grid1">
-			<h2>DERNIÈRE MODE</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo varius vehicula. Mauris porta metus vitae nulla dignissim egestas. Aliquam sed molestie eros, in pharetra purus. Praesent consequat porta viverra. Praesent gravida dapibus condimentum. Vestibulum vel lacus aliquet, laoreet mi eu, tempor est. Quisque volutpat finibus tristique. Duis at nibh eget nulla pretium pretium. Quisque arcu sem, dignissim eu egestas quis, vulputate non mauris.</p>
-			<p class="w3teaseraits">Check out our latest commercial, teaser and behind the scenes film.</p>
-			<a class="popup-with-zoom-anim" href="#small-dialog4">WATCH NOW</a>
-		</div>
-
-		<div class="col-md-6 aitscommercial-grid aitscommercial-grid2">
-			<img src="images/commercial.jpg" alt="Groovy Apparel">
-		</div>
-		<div class="clearfix"></div>
-
-		<div id="small-dialog4" class="mfp-hide agileinfo">
-			<div class="pop_up">
-				<iframe src="https://player.vimeo.com/video/184401441"></iframe>
-			</div>
-		</div>
-
-	</div>
-	<!-- //Commercial -->
-
-
-
-	<!-- Accessories -->
-	<div class="w3laccessoriesaits">
-		<div class="col-md-6 w3laccessoriesaits-grids w3laccessoriesaits-grids1">
-			<div class="w3laccessoriesaits-grid w3laccessoriesaits-grid1">
-				<div class="grid">
-					<figure class="effect-apollo">
-						<img src="images/accessory-1.jpg" alt="Groovy Apparel">
-						<figcaption></figcaption>
-					</figure>
-				</div>
-				<h4>EAU FRAICHE</h4>
-				<p>Lorem Ipsum Dolor Site Amet</p>
-				<h6><a href="mens_accessories.html">Shop Now</a></h6>
-			</div>
-		</div>
-		<div class="col-md-6 w3laccessoriesaits-grids w3laccessoriesaits-grids2">
-			<div class="w3laccessoriesaits-grid w3laccessoriesaits-grid2">
-				<div class="grid">
-					<figure class="effect-apollo">
-						<img src="images/accessory-2.jpg" alt="Groovy Apparel">
-						<figcaption></figcaption>
-					</figure>
-				</div>
-				<h4>PIGMENT POWDER</h4>
-				<p>Lorem Ipsum Dolor Site Amet</p>
-				<h6><a href="womens_accessories.html">Shop Now</a></h6>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-	<!-- //Accessories -->
-
-
-
-	<!-- Shoes -->
-	<div class="w3lsshoesaits">
-		<div class="w3lsshoesaits-info">
-			<h3>SHOES TO DIE FOR</h3>
-			<p>Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.</p>
-			<div class="wthreeshop-a">
-				<a href="womens_accessories.html">SHOP WOMEN'S SHOES</a>
-			</div>
-		</div>
-	</div>
-	<!-- //Shoes -->
-
-
-
-	<!-- New-Fashions -->
-	<div class="agilenwew3lsfashions">
-		<div class="container">
-
-			<h3>AVANT-GARDE FASHION</h3>
-			<h4>Star Studded Launching of the Festival Collection in NYFW in</h4>
-
-			<div class="agilenwew3lsfashions-aitstimer">
-				<div class="clock">
-					<div class="column days">
-						<div class="timer" id="days"></div>
-						<div class="text">DAYS</div>
-					</div>
-					<div class="timer days">:</div>
-					<div class="column">
-						<div class="timer" id="hours"></div>
-						<div class="text">HOURS</div>
-					</div>
-					<div class="timer">:</div>
-					<div class="column">
-						<div class="timer" id="minutes"></div>
-						<div class="text">MINUTES</div>
-					</div>
-					<div class="timer">:</div>
-					<div class="column">
-						<div class="timer" id="seconds"></div>
-						<div class="text">SECONDS</div>
-					</div>
-				</div>
-			</div>
-
-		</div>
-	</div>
-	<!-- //New-Fashions -->
-
-
-
-	<!-- Newsletter -->
-	<div class="w3lsnewsletter" id="w3lsnewsletter">
-		<div class="container">
-			<div class="w3lsnewsletter-grids">
-				<div class="col-md-5 w3lsnewsletter-grid w3lsnewsletter-grid-1 subscribe">
-					<p>Subscribe to our Newsletter</p>
-				</div>
-				<div class="col-md-7 w3lsnewsletter-grid w3lsnewsletter-grid-2 email-form">
-					<form action="#" method="post">
-						<input class="email" type="email" name="Email" placeholder="Email Address" required="">
-						<input type="submit" class="submit" value="SUBSCRIBE">
-					</form>
-				</div>
-				<div class="clearfix"></div>
-			</div>
-		</div>
-	</div>
-	<!-- //Newsletter -->
-
-
-
-	<!-- Footer -->
-	<div class="agileinfofooter">
-		<div class="agileinfofooter-grids">
-
-			<div class="col-md-4 agileinfofooter-grid agileinfofooter-grid1">
-				<ul>
-					<li><a href="about.html">ABOUT</a></li>
-					<li><a href="mens.html">MEN'S</a></li>
-					<li><a href="mens_accessories.html">MEN'S ACCESSORIES</a></li>
-					<li><a href="womens.html">WOMEN'S</a></li>
-					<li><a href="womens_accessories.html">WOMEN'S ACCESSORIES</a></li>
-				</ul>
-			</div>
-
-			<div class="col-md-4 agileinfofooter-grid agileinfofooter-grid2">
-				<ul>
-					<li><a href="stores.html">STORE LOCATOR</a></li>
-					<li><a href="faq.html">FAQs</a></li>
-					<li><a href="codes.html">CODES</a></li>
-					<li><a href="icons.html">ICONS</a></li>
-					<li><a href="contact.html">CONTACT</a></li>
-				</ul>
-			</div>
-
-			<div class="col-md-4 agileinfofooter-grid agileinfofooter-grid3">
-				<address>
-					<ul>
-						<li>40019 Parma Via Modena</li>
-						<li>Sant'Agata Bolognese</li>
-						<li>BO, Italy</li>
-						<li>+1 (734) 123-4567</li>
-						<li><a class="mail" href="mailto:mail@example.com">info@example.com</a></li>
-					</ul>
-				</address>
-			</div>
-			<div class="clearfix"></div>
-
-		</div>
-	</div>
-	<!-- //Footer -->
-
-
-
-	<!-- Copyright -->
-	<div class="w3lscopyrightaits">
-		<div class="col-md-8 w3lscopyrightaitsgrid w3lscopyrightaitsgrid1">
-			<p>© 2017 Groovy Apparel. All Rights Reserved | Design by <a href="http://w3layouts.com/" target="=_blank"> W3layouts </a></p>
-		</div>
-		<div class="col-md-4 w3lscopyrightaitsgrid w3lscopyrightaitsgrid2">
-			<div class="agilesocialwthree">
-				<ul class="social-icons">
-					<li><a href="#" class="facebook w3ls" title="Go to Our Facebook Page"><i class="fa w3ls fa-facebook-square" aria-hidden="true"></i></a></li>
-					<li><a href="#" class="twitter w3l" title="Go to Our Twitter Account"><i class="fa w3l fa-twitter-square" aria-hidden="true"></i></a></li>
-					<li><a href="#" class="googleplus w3" title="Go to Our Google Plus Account"><i class="fa w3 fa-google-plus-square" aria-hidden="true"></i></a></li>
-					<li><a href="#" class="instagram wthree" title="Go to Our Instagram Account"><i class="fa wthree fa-instagram" aria-hidden="true"></i></a></li>
-					<li><a href="#" class="youtube w3layouts" title="Go to Our Youtube Channel"><i class="fa w3layouts fa-youtube-square" aria-hidden="true"></i></a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-	<!-- //Copyright -->
-
-
-
-	<!-- Custom-JavaScript-File-Links -->
-<!-- Default-JavaScript --><script src="js/jquery-2.2.3.js"></script>
-<script src="js/modernizr.custom.js"></script>
-	<!-- Custom-JavaScript-File-Links -->
-
-	<!-- cart-js -->
-	<script src="js/minicart.js"></script>
-	<script>
-        w3l.render();
-
-        w3l.cart.on('w3agile_checkout', function (evt) {
-        	var items, len, i;
-
-        	if (this.subtotal() > 0) {
-        		items = this.items();
-
-        		for (i = 0, len = items.length; i < len; i++) { 
-        		}
-        	}
-        });
-    </script>  
-	<!-- //cart-js --> 
-
-
-		<!-- Dropdown-Menu-JavaScript -->
-			<script>
-				$(document).ready(function(){
-					$(".dropdown").hover(
-						function() {
-							$('.dropdown-menu', this).stop( true, true ).slideDown("fast");
-							$(this).toggleClass('open');
-						},
-						function() {
-							$('.dropdown-menu', this).stop( true, true ).slideUp("fast");
-							$(this).toggleClass('open');
-						}
-					);
-				});
-			</script>
-		<!-- //Dropdown-Menu-JavaScript -->
-
-		<!-- Popup-Box-JavaScript -->
-			<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
-			<script>
-				$(document).ready(function() {
-					$('.popup-with-zoom-anim').magnificPopup({
-						type: 'inline',
-						fixedContentPos: false,
-						fixedBgPos: true,
-						overflowY: 'auto',
-						closeBtnInside: true,
-						preloader: false,
-						midClick: true,
-						removalDelay: 300,
-						mainClass: 'my-mfp-zoom-in'
-					});
-				});
-			</script>
-		<!-- //Popup-Box-JavaScript -->
-
-		<!-- Countdown-Timer-JavaScript-File-Links -->
-			<script type="text/javascript" src="js/moment.js"></script>
-			<script type="text/javascript" src="js/moment-timezone-with-data.js"></script>
-			<script type="text/javascript" src="js/timer.js"></script>
-		<!-- //Countdown-Timer-JavaScript-File-Links -->
-
-	<!-- //Custom-JavaScript-File-Links -->
-
-	<!-- Bootstrap-JavaScript --> <script src="js/bootstrap.js"></script>
-
-</body>
-<!-- //Body -->
-
-
-
-</html>
