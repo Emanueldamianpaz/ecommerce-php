f31eef7e219616486ab779b2798f983b24e3dd2b
Emanuel.paz <emanuel.paz@despegar.com>
2017-06-25 19:58:15 -0300

    Casi termino el diseño de admin

---
+++ admin/partials/menu.php
@@ -0,0 +1,35 @@
+<div class="col-md-2">
+    <div class="sidebar content-box" style="display: block;">
+        <ul class="nav">
+            <!-- Main menu -->
+            <li class="current"><a href="index.php"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
+
+            <li class="submenu">
+                <a href="#">
+                    <i class="glyphicon glyphicon-barcode"></i> Productos
+                    <span class="caret pull-right"></span>
+                </a>
+                <!-- Sub menu -->
+                <ul>
+                    <li><a href="products.php"><i class="glyphicon glyphicon-list"></i> Listado</a></li>
+                    <li><a href="createProduct.php"><i class="glyphicon glyphicon-cloud-upload"></i> Agregar</a></li>
+                    <li><a href="deleteProduct.php"><i class="glyphicon glyphicon-remove"></i> Eliminar</a></li>
+
+                </ul>
+            </li>
+            <li class="submenu">
+                <a href="#">
+                    <i class="glyphicon glyphicon-user"></i> Usuarios
+                    <span class="caret pull-right"></span>
+                </a>
+                <!-- Sub menu -->
+                <ul>
+                    <li><a href="users.php"><i class="glyphicon glyphicon-list"></i> Listado</a></li>
+                    <li><a href="createUser.php"><i class="glyphicon glyphicon-cloud-upload"></i> Agregar</a></li>
+                    <li><a href="deleteUser.php"><i class="glyphicon glyphicon-remove"></i> Eliminar</a></li>
+
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ admin/profile.php
@@ -0,0 +1,28 @@
+<?php
+include_once("partials/header.php");
+?>
+
+
+<div class="page-content">
+    <div class="row">
+        <?php include_once("partials/menu.php"); ?>
+
+        <div class="col-md-10">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="content-box-large">
+                        <div class="panel-body">
+                            <img src="images/under_construction.jpg">
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+<?php include_once("partials/footer.php"); ?>
