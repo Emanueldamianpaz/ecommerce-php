4f7dabdf8a22e182e212373966e9cbf3c687476c
Emanuel.paz <emanuel.paz@despegar.com>
2017-06-25 19:33:50 -0300

    Works fuckin login!

--- database/login.php
+++ database/login.php
@@ -7,15 +7,11 @@ $errorMsgLogin = '';
 function verifyLogin($user, $password, &$result, $con)
 {
 
-    $sql = "SELECT * FROM users WHERE username='$user' and password='$password'";
+    $sql = "SELECT id FROM users WHERE username='$user' and password='$password'";
     $result = $con->query($sql);
-    echo $sql;
-    $count = 0;
-    while ($row = $result->fetch(PDO::FETCH_OBJ)) {
-        $count++;
-        $result = $row;
-    }
-    if ($count == 1) {
+    $row = $result->fetchAll();
+
+    if (!empty($row[0])) {
         return 1;
     } else {
         return 0;
@@ -23,24 +19,16 @@ function verifyLogin($user, $password, &$result, $con)
 }
 
 if (!isset($_SESSION['id_user'])) {
-    echo "sin sesion";
     if (isset($_POST["login"])) {
-        echo "ejecuto post";
-
         if (verifyLogin($_POST['user'], $_POST['password'], $result, $con) == 1) {
-            echo 'Su usuario ingreso correctamente.';
-
             $_SESSION['id_user'] = $result->id;
-            header("location:/admin/index.php");
+            header("location:index.php");
         } else {
             $errorMsgLogin = 'Su usuario es incorrecto, intente nuevamente.';
         }
         return;
-    } else {
-        echo "no ejecuto post";
     }
 } else {
-    echo 'Su usuario ingreso correctamente.';
     echo '<a href="logout.php">Salir</a>';
 };
 
