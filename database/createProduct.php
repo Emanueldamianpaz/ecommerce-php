18c7d4001087fce07d7836a84d4722c7b4d9a1ea
Emanuel.paz <emanuel.paz@despegar.com>
2017-06-25 22:04:40 -0300

    Seguimos con diseño de Alta

---
+++ database/createProduct.php
@@ -0,0 +1,59 @@
+<div class="panel-body">
+    <form action="" method="POST">
+        <fieldset>
+            <div class="form-group col-lg-2">
+                <label>ID</label>
+                <input class="form-control" placeholder="ID" type="text">
+            </div>
+            <div class="form-group col-lg-4">
+                <label>Name</label>
+                <input class="form-control" placeholder="Name" type="text">
+            </div>
+            <div class="form-group col-lg-2">
+                <label>Stock</label>
+                <input class="form-control" placeholder="Stock" type="text">
+            </div>
+            <div class="form-group col-lg-2">
+                <label>Price</label>
+                <input class="form-control" placeholder="Price" type="text">
+            </div>
+            <div class="form-group col-lg-2">
+                <label>Type_product</label>
+                <select class="form-control" placeholder="Type">
+                    <option>Accesorio</option>
+                    <option>Accesorio</option>
+                    <option>Accesorio</option>
+
+                    <!-- TODO Desplegable para opciones-->
+                </select>
+            </div>
+
+            <div class="form-group col-lg-6">
+                <label>Description</label>
+                <textarea style="resize: none;" class="form-control" placeholder="Description" rows="3"></textarea>
+            </div>
+            <div class="form-group col-lg-3">
+                <label>Size</label>
+                <input class="form-control" placeholder="Size" type="text">
+            </div>
+            <div class="form-group col-lg-3">
+                <label>Stars</label>
+                <select class="form-control" placeholder="Stars">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+
+                </select>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="btn btn-success col-lg-12">
+                    <i class="fa fa-save"></i>
+                    Cargar producto
+                </div>
+            </div>
+        </fieldset>
+    </form>
+</div>
